ba0d1b4de27a14af4ae307843a4a2a1dded41a69
Kuwe <kai.sternke@haw-hamburg.de>
2018-11-26 10:07:56 +0100

    Update lstm.py

--- make-a-sound/Classical-Piano-Composer/lstm.py
+++ make-a-sound/Classical-Piano-Composer/lstm.py
@@ -119,7 +119,7 @@ def train(model, network_input, network_output):
 
     callbacks_list = [checkpoint,accuracy, TerminateOnNaN()]
 
-    history = model.fit(network_input, network_output, epochs=200, batch_size=10000, callbacks=callbacks_list)
+    history = model.fit(network_input, network_output, epochs=200, batch_size=2500, callbacks=callbacks_list)
     print(history.history.keys)
 
 if __name__ == '__main__':
