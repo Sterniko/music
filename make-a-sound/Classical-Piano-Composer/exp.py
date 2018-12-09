42e506e3e72360c96bfa67f978dd75aee7a15cae
Sterniko <33185467+Sterniko@users.noreply.github.com>
2018-12-09 21:39:21 +0100

    Update exp.py

--- make-a-sound/Classical-Piano-Composer/exp.py
+++ make-a-sound/Classical-Piano-Composer/exp.py
@@ -56,7 +56,7 @@ def get_notes():
 
 def prepare_sequences(notes, n_vocab):
     """ Prepare the sequences used by the Neural Network """
-    sequence_length = 100
+    sequence_length = 50
 
     # get all pitch names
     pitchnames = sorted(set(item for item in notes))
