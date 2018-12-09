2230c47ec5249e6b50c4859d21c46f02662a2da1
Sterniko <33185467+Sterniko@users.noreply.github.com>
2018-12-09 19:44:27 +0100

    Update predict.py

--- make-a-sound/Classical-Piano-Composer/predict.py
+++ make-a-sound/Classical-Piano-Composer/predict.py
@@ -60,10 +60,10 @@ def create_network(network_input, n_vocab):
         return_sequences=True
     ))
     model.add(Dropout(0.3))
-    model.add(LSTM(512, return_sequences=True))
+    model.add(LSTM(512*2, return_sequences=True))
     model.add(Dropout(0.3))
-    model.add(LSTM(512))
-    model.add(Dense(256))
+    model.add(LSTM(512*2))
+    model.add(Dense(256*2))
     model.add(Dropout(0.3))
     model.add(Dense(n_vocab))
     model.add(Activation('softmax'))
