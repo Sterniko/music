f8093674548b0d7118354cb409abd0ae74811ed4
Sterniko <33185467+Sterniko@users.noreply.github.com>
2018-12-09 20:37:13 +0100

    Update predict.py

--- make-a-sound/Classical-Piano-Composer/predict.py
+++ make-a-sound/Classical-Piano-Composer/predict.py
@@ -54,16 +54,18 @@ def create_network(network_input, n_vocab):
     print("create network")
     """ create the structure of the neural network """
     model = Sequential()
+    notessize = len(notes)
     model.add(LSTM(
-        512,
+        notessize,
         input_shape=(network_input.shape[1], network_input.shape[2]),
         return_sequences=True
     ))
+    
     model.add(Dropout(0.3))
-    model.add(LSTM(512*2, return_sequences=True))
+    model.add(LSTM(notessize, return_sequences=True))
     model.add(Dropout(0.3))
-    model.add(LSTM(512*2))
-    model.add(Dense(256*2))
+    model.add(LSTM())
+    model.add(Dense(notessize))
     model.add(Dropout(0.3))
     model.add(Dense(n_vocab))
     model.add(Activation('softmax'))
