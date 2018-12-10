bda417ead051a6eb2c3a16be1709f222623de87f
Sterniko <33185467+Sterniko@users.noreply.github.com>
2018-12-10 09:57:57 +0100

    Update exp.py

--- make-a-sound/Classical-Piano-Composer/exp.py
+++ make-a-sound/Classical-Piano-Composer/exp.py
@@ -105,20 +105,20 @@ def create_network(network_input, n_vocab, noteslength):
     model = Sequential()   
     print("model.add")
     model.add(LSTM(
-        noteslength,
+        int(noteslength/2),
         input_shape = (network_input.shape[1], network_input.shape[2]),
         return_sequences=True
     ))
     print("dropout")
     model.add(Dropout(0.3))
     print("LSTM")
-    model.add(LSTM(noteslength, return_sequences=True))
+    model.add(LSTM( int(noteslength/2), return_sequences=True))
     print("dropout")
     model.add(Dropout(0.3))
     print("LSTM")
-    model.add(LSTM(noteslength))
+    model.add(LSTM( int(noteslength/2)))
     print("Dense")
-    model.add(Dense(noteslength/2))
+    model.add(Dense( int(noteslength/4)))
     print("dropout")
     model.add(Dropout(0.3))
     print("Dense")
